33d19738b1f96d38dde446897005d4546f656ba6
JuanpaGarcia <garciaperezjuanpablo7@gmail.com>
2022-10-15 22:06:02 -0500

    Test code for sensors and actuators

--- IoT_Project/IoT_Project.ino
+++ IoT_Project/IoT_Project.ino
@@ -10,18 +10,21 @@
 ////////////// Definitions ////////////////////////////////////
 
 #define LDR_PIN  15
-#define T_Sensor_PIN 16
-#define RELE_PIN 18
-#define PH_SENSOR_PIN 14
-#define SERVO_PIN 19
+#define T_Sensor_PIN 27
+#define RELE_PIN 4
+#define PH_SENSOR_PIN 33
+#define SERVO_PIN 2
 
 //I2C pins
 #define SDA_PIN 21
 #define SCL_PIN 22
 #define SLAVE_ADDR 0x05
 
-#define ADC_mV 3300
+#define LDR_REF_RESISTOR 10000 //value for reference resistor in ohms
+
+#define ADC_mV 5000
 #define ADC_Res 4096
+#define MAX_VALUE_PH  3475    //Max possible value obtained from an ADC read
 
 #define  sensors_buffer_size  3 //buffer size for sensors to be stored
 #define  LDR_buffer_position 0
@@ -32,7 +35,6 @@
 
 ////////////////////// Tasks /////////////////////////
 void Task_LDR_Read( void *pvParameters );
-void Task_Serial_Print( void *pvParameters );
 void Task_Read_Temperature( void *pvParameters );
 void Task_Rele( void *pvParameters );
 void Task_pH( void *pvParameters );
@@ -51,7 +53,7 @@ void setup() {
   feed_fish = xSemaphoreCreateBinary();
   
   // initialize serial communication at 115200 bits per second:
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   // Task to run forever
   xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
@@ -60,16 +62,6 @@ void setup() {
               1024,         // Stack size (bytes in ESP32, words in FreeRTOS)
               NULL,         // Parameter to pass to function
               1,            // Task priority (0 to configMAX_PRIORITIES - 1)
-              NULL,         // Task handle
-              ARDUINO_RUNNING_CORE);     // Run on one core for demo purposes (ESP32 only)
-
-  // Task to run forever
-  xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
-              Task_Serial_Print,  // Function to be called
-              "Print data to serial terminal",   // Name of task
-              2048,         // Stack size (bytes in ESP32, words in FreeRTOS)
-              NULL,         // Parameter to pass to function
-              2,            // Task priority (0 to configMAX_PRIORITIES - 1)
               NULL,         // Task handle
               ARDUINO_RUNNING_CORE);     // Run on one core for demo purposes (ESP32 only)
 
@@ -145,15 +137,18 @@ void Task_LDR_Read( void *pvParameters )
     int LDR_a_read_value = analogRead(LDR_PIN);
     //Process data 
 
-    //store data
-    Global_variable_buffer[0] = LDR_a_read_value;
+    //pre proces data
+    int light = map(LDR_a_read_value, 0, ADC_Res, 0, 100);
+    
  
     // print the temperature in the Serial Monitor:
     Serial.print("LDR_Value: ");
-    Serial.print(LDR_a_read_value);   // print the temperature in °C
+    Serial.print(LDR_a_read_value );   // print the temperature in °C
+    Serial.print("LDR_brightness: ");
+    Serial.print(light );   // brightness
     Serial.println("end LDR");
     
-    vTaskDelay(10);  // one tick delay (15ms) in between reads for stability
+    vTaskDelay(100);  // one tick delay (15ms) in between reads for stability
   }
 }
 
@@ -165,7 +160,6 @@ void Task_Read_Temperature( void *pvParameters )
   Reads an analog input on pin T_sensor in defines and stores the value in a global arry for later be printed in a serial terminal
   This example code is in the public domain.
 */
-
   for (;;)
   {
     // read the input on analog pin LDR_PIN:
@@ -180,27 +174,26 @@ void Task_Read_Temperature( void *pvParameters )
     Serial.print("Temperature: ");
     Serial.print(Temp);   // print the temperature in °C
     Serial.println("°C");
-    
-    vTaskDelay(10);  // one tick delay (15ms) in between reads for stability
+    vTaskDelay(100);  // one tick delay (15ms) in between reads for stability
   }
 }
 
 void Task_Rele( void *pvParameters )
 {
-    //make statements and semaphores for this
     pinMode(RELE_PIN, OUTPUT);
     for (;;)
   {
-    if (NULL != rele_semaphore)
-    {
-      if( pdTRUE == xSemaphoreTake( rele_semaphore, ( TickType_t ) 10 )) //check the semaphor to take it or wait n ticks to see if it becomes available
-      {
-          digitalWrite(RELE_PIN, HIGH);   // turn the Rele on (HIGH is the voltage level)
-          vTaskDelay(100);  // one tick delay (15ms) in between reads for stability
-          digitalWrite(RELE_PIN, LOW);    // turn the Rele off by making the voltage LOW
-      }
-    }
-    vTaskDelay(100);  // one tick delay (15ms) in between reads for stability
+      digitalWrite(RELE_PIN, LOW);
+      Serial.println("Current Flowing in Rele");
+      vTaskDelay(300);  // one tick delay (15ms) in between reads for stability 
+      
+      // Normally Open configuration, send HIGH signal stop current flow
+      // (if you're usong Normally Closed configuration send LOW signal)
+      digitalWrite(RELE_PIN, HIGH);
+      Serial.println("Current not Flowing in rele");
+      vTaskDelay(300);  // one tick delay (15ms) in between reads for stability
+
+    
   }
 }
 
@@ -212,15 +205,15 @@ void Task_feed_fish( void *pvParameters )
     myservo.attach(SERVO_PIN);
     for (;;)
   {
-    if (NULL != feed_fish)
-    {
-      if( pdTRUE == xSemaphoreTake( feed_fish, ( TickType_t ) 10 )) //check the semaphor to take it or wait n ticks to see if it becomes available
-      {
-        //take actions to feed fish 
-        myservo.write(pos); 
-      }
+      for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+      // in steps of 1 degree
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+      vTaskDelay(10);  // one tick delay (15ms) in between reads for stability
     }
-    vTaskDelay(100);  // one tick delay (15ms) in between reads for stability
+    for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+      vTaskDelay(10);  // one tick delay (15ms) in between reads for stability
+    }
   }
 }
 
@@ -236,32 +229,18 @@ void Task_pH( void *pvParameters )
   for (;;)
   {
     // read the input on analog pin LDR_PIN:
-    int pH_a_read_value = analogRead(PH_SENSOR_PIN);
+    static int pH_a_read_value = analogRead(PH_SENSOR_PIN);
     //Process data 
-    int temperature = 0, averageVoltage = 0;
-    
-    float pH_voltage =  pH_a_read_value *3.3 /ADC_Res;
-    float compensationCoefficient = 1.0 + 0.02 * (temperature - 25.0);
-    float compensationVolatge = averageVoltage / compensationCoefficient; //temperature compensation
-    float ph = (133.42 * compensationVolatge * compensationVolatge * compensationVolatge - 255.86 * compensationVolatge * compensationVolatge + 857.39 * compensationVolatge) * 0.5; //convert voltage value to tds value
+
+    static int water_quality = map(pH_a_read_value, 0, MAX_VALUE_PH, 100, 0);
 
     //store data
-    
+     
     // print the temperature in the Serial Monitor:
-    Serial.print("Ph: ");
-    Serial.print(ph);   // print the temperature in °C
-    Serial.println("end");
+       
+    Serial.print("Calidad agua: ");
+    Serial.println(water_quality);
     
-    vTaskDelay(10);  // one tick delay (15ms) in between reads for stability
+    vTaskDelay(100);  // one tick delay (15ms) in between reads for stability
   }
-}
-
-void Task_Serial_Print( void *pvParameters )
-{
-  /*
-  serial print of data adquired in a terminal
-*/
-  //Serial.print("Data adquired from the LDR -> ");
-  
-  vTaskDelay(50);  // one tick delay (15ms) in between reads for stability
 }
